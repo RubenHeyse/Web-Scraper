fc657934dc4ca0ae684a991591a1975375dc961e
RubenHeyse <50083619+RubenHeyse@users.noreply.github.com>
2019-04-28 20:56:37 +0100

    Add files via upload

---
+++ SenecaLogin.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import pyautogui as pag
+import sys
+import time
+
+browser = webdriver.Chrome(ChromeDriverManager().install())
+browser.get('https://app.senecalearning.com/login')
+time.sleep(1)
+
+browser.find_element_by_xpath('//*[@id="root"]/div[1]/div[3]/div/div/div/div/form/div[1]/input').send_keys('13rheyse@mgs.kent.sch.uk')
+browser.find_element_by_xpath('//*[@id="root"]/div[1]/div[3]/div/div/div/div/form/div[2]/div/input').send_keys('Xm94?@8Saq6#(')
+browser.find_element_by_xpath('//*[@id="root"]/div[1]/div[3]/div/div/div/div/form/button').click()
